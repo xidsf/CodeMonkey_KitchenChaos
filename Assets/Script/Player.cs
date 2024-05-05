ed8ca6715f3a5c19f1f35286aa22adb0d357505a
xidsf <gooderic816053@gmail.com>
2024-05-05 20:59:37 +0900

    Collision Detection
    Collision Detection

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -16,7 +16,40 @@ public class Player : MonoBehaviour
         Vector2 inputVector = gameInput.GetMovementVectorNormarlized();
 
         Vector3 moveDir = new Vector3(inputVector.x, 0, inputVector.y);
-        transform.position += moveDir * moveSpeed * Time.deltaTime;
+
+        float moveDistance = moveSpeed * Time.deltaTime;
+        float playerRadius = .7f;
+        float playerHeight = 2f;
+        bool canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius , moveDir, moveDistance);
+        if(!canMove)
+        {
+            //cannot move towrds moveDir;
+
+            //Attapy only x movenemt
+            Vector3 moveDirX = new Vector3(moveDir.x, 0, 0).normalized;
+            canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirX, moveDistance);
+            if(canMove)
+            {
+                moveDir = moveDirX;
+            }
+            else
+            {
+                Vector3 moveDirZ = new Vector3(0, 0, moveDir.z).normalized;
+                canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirZ , moveDistance);
+                if (canMove)
+                {
+                    moveDir = moveDirZ;
+                }
+                else
+                {
+
+                }
+            }
+        }
+        if(canMove )
+        {
+            transform.position += moveDir * moveDistance;
+        }
 
         isWalking = (moveDir != Vector3.zero);
 
